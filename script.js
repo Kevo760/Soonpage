a2483baa4e4bcbb3eb34b900b44deaca48609c6b
TheLegend760 <73809530+TheLegend760@users.noreply.github.com>
2021-08-14 23:40:56 -0700

    Final Ver. 1.0.0
    -Fixed Javascript

---
+++ script.js
@@ -0,0 +1,27 @@
+const form = document.querySelector('form');
+const button = form.querySelector('[type="submit"]');
+const small = document.querySelector('small');
+
+function checkEmail(e){
+  e.preventDefault();
+  const email = e.target.querySelector('[type="email"]').value;
+  if(!validateEmail(email)) {
+    form.classList.add('error');
+    small.style.display = 'inline-block';
+    console.log("error");
+  } else {
+    form.classList.remove('error');
+    small.style.display = "none";
+    console.log("noterror");
+  }
+  console.log(email);
+}
+
+
+function validateEmail(email) {
+    const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
+}
+
+
+form.addEventListener('submit', checkEmail);
